28821a5605a11b5b4d0429d9f3fd8c758f2b27c6
Thuy Tien Nguyen <35412770+juniHub@users.noreply.github.com>
2023-05-01 06:44:53 -0700

    fix testing

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -2,16 +2,18 @@ public class RewardValue {
     private double cashValue;
     private int milesValue;
 
+    public static final double MILES_TO_CASH_RATE = 0.0035;
+
     // Constructor that accepts a cash value
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
-        this.milesValue = (int) (cashValue / 0.0035);
+        this.milesValue = (int) (cashValue / MILES_TO_CASH_RATE);
     }
 
     // Constructor that accepts a value in miles
     public RewardValue(int milesValue) {
         this.milesValue = milesValue;
-        this.cashValue = milesValue * 0.0035;
+        this.cashValue = milesValue * MILES_TO_CASH_RATE;
     }
 
     // Method that returns the cash value of the RewardValue
